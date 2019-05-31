79b4fec7ec118f1667329a7a1f448fcdbfa630ad
Stefan Majer <stefan.majer@f-i-ts.de>
2019-05-31 11:06:36 +0200

    Add a simple full cycle test

---
+++ cmd/go.mod
@@ -0,0 +1,5 @@
+module github.com/metal-pod/v/cmd
+
+go 1.12
+
+require github.com/metal-pod/v v0.0.1
---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/metal-pod/v"
+)
+
+func main() {
+	const expected = "v0.0.1 (abcdef), devel, 1.1.1970"
+	version := v.V.String()
+	if version != expected {
+		panic("expected:" + expected + " got:" + version)
+	}
+}
